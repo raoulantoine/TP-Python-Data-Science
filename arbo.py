b3e1290a353d1c7f5adafa7eb7a8edd3477246b5
raoulantoine <antoine.raoul@live.fr>
2021-01-12 09:36:28 +0100

    première partie

--- arbo.py
+++ arbo.py
@@ -1,6 +1,17 @@
-import os
+import re
 import csv
+import os
+
 with open('departements-france.csv', newline='') as csvfile:
-    spamreader = csv.reader(csvfile, delimiter=' ', quotechar='|')
-    for row in spamreader:
-        print(', '.join(row))
+    reader = csv.DictReader(csvfile)
+    for row in reader:
+        # creation des dossiers Region
+        if not os.path.exists("cible/"+row['nom_region']):
+            os.makedirs("cible/"+row['nom_region'])
+            print("create folder " + row['nom_region'])
+
+        # création des dossiers département dans les dossiers region
+        if not os.path.exists("cible/"+row['nom_region']+"/"+row['nom_departement']):
+            os.makedirs("cible/"+row['nom_region']+"/"+row['nom_departement'])
+            print("create folder " + row['nom_departement'] + " create in " +
+                  row['nom_region'])
